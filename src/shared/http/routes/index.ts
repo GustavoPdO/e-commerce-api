6fe0772b2d7765af3c34566fd673260239d897e8
Gustavo Pedralino <gpedralino@gmail.com>
2021-03-23 23:16:18 -0300

    Section 6: create users table migration, entity, repository, create and list services, controller and routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,14 @@
+import { request, response, Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
-import { Router } from 'express';
+import usersRouter from '@modules/users/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
+routes.use('/users', usersRouter);
+
+routes.get('/', (request, response) => {
+  return response.json('oi');
+});
 
 export default routes;
